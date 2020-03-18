1c0f732dbf8790f36fbc6bf7ab6bb1508305aaba
Lenny Esquivel <56128925+lennyesquivel-hexaware@users.noreply.github.com>
2020-03-18 14:33:18 -0600

    fix localhost heroku

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -16,7 +16,7 @@ export class ProjectsService {
   setProject(name: string, description: string, start_date: any, end_date: any, image: any) {
     const project: Project = { name, description, start_date, end_date, image };
     let nameerror = false;
-    this.http.post<{error: boolean, message: string}>('http://localhost:3000/api/postproject', project).subscribe((responseData) => {
+    this.http.post<{error: boolean, message: string}>('localhost:3000/api/postproject', project).subscribe((responseData) => {
       alert(responseData.message);
       if (responseData.error) {
         nameerror = true;
@@ -27,21 +27,21 @@ export class ProjectsService {
 
   getProject(name: string) {
     const projname = { name };
-    return this.http.post('http://localhost:3000/api/lastproject', projname).toPromise();
+    return this.http.post('localhost:3000/api/lastproject', projname).toPromise();
   }
 
   setMember(projid: number, user: number, role: string) {
     const member = { projid, user, role };
-    return this.http.post('http://localhost:3000/api/postmember', member).toPromise();
+    return this.http.post('localhost:3000/api/postmember', member).toPromise();
   }
 
   getUserProjects(userid: string) {
     const usid = { userid };
-    return this.http.post('http://localhost:3000/api/userprojs', usid).toPromise();
+    return this.http.post('localhost:3000/api/userprojs', usid).toPromise();
   }
 
   getProjectInfo(projectid: string) {
     const proj = { projectid };
-    return this.http.post('http://localhost:3000/api/projectinfo', proj).toPromise();
+    return this.http.post('localhost:3000/api/projectinfo', proj).toPromise();
   }
 }
